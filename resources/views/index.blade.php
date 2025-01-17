d0f7ed11ac459d32b1aaf03321506e84a04e6310
oktaviani dwi nur aini <oktaviadna507@gmail.com>
2025-01-17 16:54:45 +0700

    front end 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,33 +30,35 @@
 	<!-- Start Header/Navigation -->
 	<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
-		<div class="container">
-			<a class="navbar-brand" href="/">Furni<span>.</span></a>
+	<div class="container">
+    <a class="navbar-brand" href="/">
+        <img src="{{ asset('images/logo-sarinah.png') }}" alt="Logo Mall Sarinah" style="height: 50px; margin-left: 20px;">
+    </a>
+</div>
 
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-			<div class="collapse navbar-collapse" id="navbarsFurni">
-				<ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-					<li class="nav-item active">
-						<a class="nav-link" href="/">Home</a>
-					</li>
-					<li><a class="nav-link" href="shop">Shop</a></li>
-					<li><a class="nav-link" href="about">About us</a></li>
-					<li><a class="nav-link" href="services">Services</a></li>
-					<li><a class="nav-link" href="blog">Blog</a></li>
-					<li><a class="nav-link" href="contact">Contact us</a></li>
-				</ul>
+        <div class="collapse navbar-collapse" id="navbarsFurni">
+            <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li><a class="nav-link" href="shop">Shop</a></li>
+                <li><a class="nav-link" href="about">About </a></li>
+                <li><a class="nav-link" href="services">Services</a></li>
+                <li><a class="nav-link" href="blog">Blog</a></li>
+                <li><a class="nav-link" href="contact">Contact </a></li>
+            </ul>
 
-				<ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-					<li><a class="nav-link" href="#"><img src="images/user.svg"></a></li>
-					<li><a class="nav-link" href="cart.html"><img src="images/cart.svg"></a></li>
-				</ul>
-			</div>
-		</div>
-
-	</nav>
+            <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+                <li><a class="nav-link" href="#"><img src="images/user.svg" alt="User"></a></li>
+                <li><a class="nav-link" href="cart.html"><img src="images/cart.svg" alt="Cart"></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 	<!-- End Header/Navigation -->
 
 	<!-- Start Hero Section -->
